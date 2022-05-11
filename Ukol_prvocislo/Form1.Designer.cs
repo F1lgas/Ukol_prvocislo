cc19b2f4a7041272d70b0b797208b9df5d47e931
Filip Iran <iranf.03@spst.cz>
2022-05-11 19:47:28 +0200

    v1.0

--- Ukol_prvocislo/Form1.Designer.cs
+++ Ukol_prvocislo/Form1.Designer.cs
@@ -28,13 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.zjistit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // zjistit
+            // 
+            this.zjistit.Location = new System.Drawing.Point(192, 10);
+            this.zjistit.Name = "zjistit";
+            this.zjistit.Size = new System.Drawing.Size(75, 23);
+            this.zjistit.TabIndex = 0;
+            this.zjistit.Text = "Zjistit";
+            this.zjistit.UseVisualStyleBackColor = true;
+            this.zjistit.Click += new System.EventHandler(this.zjistit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Zadej N:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(86, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 89);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 39);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Zadej\r\nN-prvkovou\r\nposloupnost:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(276, 138);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zjistit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Úkol prvočíslo";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button zjistit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Ukol_prvocislo/Form1.cs
+++ Ukol_prvocislo/Form1.cs
@@ -16,5 +16,55 @@ namespace Ukol_prvocislo
         {
             InitializeComponent();
         }
+
+        private void zjistit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (numericUpDown1.Value != textBox1.Lines.Count())
+                {
+                    MessageBox.Show("Zadal jsi jiný počet prvků než N!", "Error");
+                    return;
+                }
+
+                foreach (string radek in textBox1.Lines)
+                {
+                    int cislo = Convert.ToInt32(radek);
+
+                    if (cislo >= 5 && cislo <= 120)
+                    {
+                        if (prvocislo(cislo))
+                        {
+                            MessageBox.Show("První prvočíslo ze zadaných čísel je " + cislo, "Výsledek");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Některé číslo se nenachází v intervalu <5;120>!", "Error");
+                        return;
+                    }
+                }
+                MessageBox.Show("V posloupnosti se nenachází žádné prvočíslo!", "Výsledek");
+            }
+            catch
+            {
+                MessageBox.Show("Zadal jsi špatné údaje!");
+                return;
+            }
+        }
+
+        private bool prvocislo (int cislo)
+        {
+            double cislo2 = cislo / 2;
+            for (int i = 2; i <= cislo2; i++)
+            {
+                if (cislo % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
